82bb2d48279a096563ed95b536f276b006e013a3
Bruno Santos <103156528+Ta10Bruno@users.noreply.github.com>
2023-10-08 17:11:34 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+
+if(window.Swiper){
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'vertical',
+      loop: true,
+        autoplay: {
+        delay: 4000,
+      },
+    });
+  }
+
